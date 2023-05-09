f17ee20874ab7e581f785335a85977a0af9d5451
Fuzereal(Артем) <112430536+fuzereal@users.noreply.github.com>
2023-05-09 13:27:15 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
 
+let counter = 1;
 let item = "";
+document.getElementById("count").innerHTML = counter + 'ШТ';
 
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
@@ -13,24 +15,34 @@ let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+let btn10 = document.getElementById("btn10");
+let btn11 = document.getElementById("btn11");
+let btn12 = document.getElementById("btn12");
+let btn15 = document.getElementById("btn15");
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
+document.getElementById('btn1').onclick = function() {
+	window.location.href = 'btn1.html';
+  };
+
+document.getElementById('btn1').onclick = function() {
+	window.location.href = 'btn1.html';
+  };
+
+
+
+  btn15.addEventListener("click", function(){
+	counter += 1;
 });
-
 btn2.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
+		
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
+		tg.MainButton.setText("Товар был добавлен в коризну!");
 		item = "2";
 		tg.MainButton.show();
 	}
@@ -41,7 +53,7 @@ btn3.addEventListener("click", function(){
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
+		tg.MainButton.setText("Товар был добавлен в коризну!");
 		item = "3";
 		tg.MainButton.show();
 	}
@@ -52,7 +64,7 @@ btn4.addEventListener("click", function(){
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
+		tg.MainButton.setText("Товар был добавлен в коризну!");
 		item = "4";
 		tg.MainButton.show();
 	}
@@ -63,7 +75,7 @@ btn5.addEventListener("click", function(){
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
+		tg.MainButton.setText("Товар был добавлен в коризну!");
 		item = "5";
 		tg.MainButton.show();
 	}
@@ -74,12 +86,69 @@ btn6.addEventListener("click", function(){
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
+		tg.MainButton.setText("Товар был добавлен в коризну!");
 		item = "6";
 		tg.MainButton.show();
 	}
 });
-
+btn7.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Товар был добавлен в коризну!");
+		item = "6";
+		tg.MainButton.show();
+	}
+});
+btn8.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Товар был добавлен в коризну!");
+		item = "6";
+		tg.MainButton.show();
+	}
+});
+btn9.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Товар был добавлен в коризну!");
+		item = "6";
+		tg.MainButton.show();
+	}
+});btn10.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Товар был добавлен в коризну!");
+		item = "6";
+		tg.MainButton.show();
+	}
+});btn11.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Товар был добавлен в коризну!");
+		item = "6";
+		tg.MainButton.show();
+	}
+});
+btn12.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Товар был добавлен в коризну!");
+		item = "6";
+		tg.MainButton.show();
+	}
+});
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
@@ -94,7 +163,6 @@ ${tg.initDataUnsafe.user.last_name}`;
 
 
 usercard.appendChild(p); 
-
 
 
 
